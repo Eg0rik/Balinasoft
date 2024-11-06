6f775c7fecfd2c01e903e2ad26eadafc7f18be73
Egor <eg887or@gmail.com>
2024-11-06 15:55:31 +0300

    [*] убрал storyboard-ы

---
+++ Balinasoft/StartViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Balinasoft
+//
+//  Created by MAC on 11/5/24.
+//
+
+import UIKit
+
+final class StartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+}
+
+private extension StartViewController {
+    func setupView() {
+        view.backgroundColor = .systemYellow
+    }
+}
