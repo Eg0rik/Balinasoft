ba2b9cca56f573cc581305148e494701a91e5c89
Egor <eg887or@gmail.com>
2024-11-06 16:15:41 +0300

    [+] добавил multipart request, отпправляет картинку с типом image/jpeg

--- Balinasoft/Services/NetworkService.swift
+++ Balinasoft/Services/NetworkService.swift
@@ -23,4 +23,35 @@ final class NetworkService {
             completion(response.result)
         }
     }
+    
+    func uploadImage(image: UIImage, id: Int, name: String, messageCompletion: @escaping (String) -> Void) {
+        
+        let urlString = "https://junior.balinasoft.com/api/v2/photo"
+        
+        guard
+            let nameData = name.data(using: .utf8),
+            let idData = id.description.data(using: .utf8),
+            let imageData = image.jpegData(compressionQuality: 0.1)
+        else {
+            print("\(#function) - can't transform values to data using .utf8")
+            return
+        }
+        
+        let fileName = "image_\(UUID()).jpeg"
+        
+        AF.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(nameData, withName: "name")
+            multipartFormData.append(idData, withName: "typeId")
+            multipartFormData.append(imageData, withName: "photo",fileName: fileName, mimeType: "image/jpeg")
+            
+        },to: urlString, method: .post).response { aFDataResponse in
+            if let statusCode = aFDataResponse.response?.statusCode,
+                statusCode == 200 {
+                messageCompletion("The \(fileName) was successfully sent with status code \(statusCode)")
+            } else {
+                messageCompletion("Error when sending multipartFormData to server, look at console")
+                debugPrint(aFDataResponse)
+            }
+        }
+    }
 }
